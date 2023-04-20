4098f9bc261cf5e7da36a77f57e846370d2bd270
luciendupont <lucienluludupont@gmail.com>
2023-04-20 13:25:49 +0200

    cli

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,18 +3,27 @@
 namespace App\Controller;
 
 use App\Repository\CategorieRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 
 class CategorieController extends AbstractController
 {
     #[Route('/categorie', name: 'app_categorie')]
-    public function categorie(CategorieRepository $categorieRepository): Response
+    public function categorie(CategorieRepository $categoriripo,  PaginatorInterface $paginator, Request $request): Response
     {
-            $cat=$categorieRepository->findAll();
+        $cat = $paginator->paginate(
+            $categoriripo->findAll(),
+            $request->query->getInt('page', 1),
+            7
+        );
         return $this->render('categorie/index.html.twig', [
             'cats'=>$cat
         ]);
     }
+
+
+    
 }
--- src/Controller/PlatController.php
+++ src/Controller/PlatController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\PlatRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PlatController extends AbstractController
 {
     #[Route('/plat', name: 'app_plat')]
-    public function plat(PlatRepository $platRepository): Response
+    public function plat(PlatRepository $platRepository,  PaginatorInterface $paginator, Request $request): Response
     {
-        $plat=$platRepository->findAll();
+        $plat = $paginator->paginate(
+            $platRepository->findAll(),
+            $request->query->getInt('page', 1),
+            7
+        );
         return $this->render('plat/index.html.twig', [
          'plats'=>$plat
         ]);
