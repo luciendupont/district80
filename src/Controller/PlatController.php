f898c60731ce2d47840af757f5dabf0065583d3a
luciendupont <lucienluludupont@gmail.com>
2023-05-22 16:25:45 +0200

    cli

--- src/Controller/PlatController.php
+++ src/Controller/PlatController.php
@@ -23,7 +23,7 @@ class PlatController extends AbstractController
         $plat = $paginator->paginate(
             $platRepository->findAll(),
             $request->query->getInt('page', 1),
-            1
+            4
         );
         return $this->render('plat/index.html.twig', [
          'plats'=>$plat
